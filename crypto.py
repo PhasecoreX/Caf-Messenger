c1e9119f769fc0b311bfc709c349cf41b73b1598
Ryan Foster <phasecorex@gmail.com>
2014-10-01 23:39:33 -0400

    Code is now 100% more readable, with docstrings and such.

--- crypto.py
+++ crypto.py
@@ -1,155 +1,220 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  test_rsa.py
-#  
+#  crypto.py
+#
 #  Copyright 2014 Ryan Foster <phasecorex@gmail.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
-from Crypto.PublicKey import RSA
-from Crypto.Cipher import AES
+'''crypto.py
+
+A bunch of functions for encryption in Cafe Messenger
+'''
+
 from Crypto import Random
-from Crypto.Signature import PKCS1_PSS
+from Crypto.Cipher import AES
 from Crypto.Hash import SHA
+from Crypto.PublicKey import RSA
+from Crypto.Signature import PKCS1_PSS
+import logging
 
-'''
-Generates a public/private 4096 bit RSA key
-Returns an RSA key object (_RSAobj) containing the full key pair
-'''
+
+logging.basicConfig(format='%(asctime)s %(levelname)s: %(message)s',
+                    datefmt='%m/%d/%Y %I:%M:%S %p')
+logging.basicConfig(filename='/home/ryan/Desktop/example.log',
+                    filemode='w', level=logging.DEBUG)
+# logging.warning('is when this event was logged.')
+
+
 def generate_key_pair():
+    """Generates a public/private 4096 bit RSA key
+
+    Returns:
+        RSA key object (_RSAobj) containing the full key pair
+    """
     return RSA.generate(4096)
 
-'''
-Extracts the public key from a full key pair
-Returns an RSA key object (_RSAobj) containing only the public key
----
-private_key - RSA key object (_RSAobj) containing users private and public key
-'''
+
 def get_public_key(private_key):
+    """Extracts the public key from a full key pair
+
+    Args:
+        private_key: RSA key object (_RSAobj) containing a private/public key
+
+    Returns:
+        RSA key object (_RSAobj) containing only the public key
+    """
     return private_key.publickey()
 
+
 def get_public_key_string(private_key):
+    """Extracts the public key from a full key pair
+
+    Args:
+        private_key: RSA key object (_RSAobj) containing a private/public key
+
+    Returns:
+        String representing public key
+    """
     return private_key.publickey().exportKey('PEM')
 
-'''
-Saves key to encrypted user.pem file
-Returns True on success, False otherwise
----
-key      - RSA key object (_RSAobj) to write to disk
-location - Path of folder to save to
----
-NOT DONE YET!
-'''
+
 def save_full_key(key, keyfile_path, password):
-    exportedKey = key.exportKey('PEM', password, pkcs=1)
+    """Saves key to encrypted user.pem file
+
+    Args:
+        key:      RSA key object (_RSAobj) to write to disk
+        location: Path of folder to save to
+
+    Returns:
+        True on success, False otherwise
+
+    Raises:
+        IOError: An error occurred accessing the bigtable.Table object.
+    """
+    exported_key = key.exportKey('PEM', password, pkcs=1)
     try:
-        f = open(keyfile_path,'w')
-        f.write(exportedKey)
-        f.close()
+        file_obj = open(keyfile_path, 'w')
+        file_obj.write(exported_key)
+        file_obj.close()
         return True
-    except:
+    except IOError:
         return False
 
-'''
-Loads key from .pem file
-Returns RSA key object (_RSAobj) containing 
----
-file - Full path and file name of .pem key
-'''
+
 def load_key(keyfile_path, password):
+    '''Loads key from .pem file
+
+    Will work with both public and private key files
+
+    Args:
+        file: Full path and file name of .pem key
+
+    Returns:
+        RSA key object (_RSAobj) containing public and/or private key
+    '''
     try:
         return RSA.importKey(open(keyfile_path).read(), password)
     except:
         raise
 
-'''
-Encrypts data using RSA key (used for authentication)
-Returns encrypted result asymmetric key - tuple list(?)
----
-pub_key   - RSA key object (_RSAobj) containing public key of recipient, used for encrypting
-plaintext - Text to encrypt
-'''
+
 def encrypt_auth(pub_key, plaintext):
+    '''Encrypts data using RSA key (used for authentication)
+
+    Args:
+        pub_key:   RSA key object (_RSAobj) containing public key of recipient,
+                   used for encrypting
+        plaintext: Text to encrypt
+
+    Returns:
+        Encrypted result asymmetric key - tuple list(?)
+    '''
     rng = Random.new()
     return pub_key.encrypt(plaintext, rng.read(8192))
 
-'''
-Decrypts data using RSA (used for authentication)
-Returns decrypted message - string
----
-private_key - RSA key object (_RSAobj) containing users private key, used for decrypting
-ciphertext  - Tuple list(?) representing encrypted message
-'''
+
 def decrypt_auth(private_key, ciphertext):
+    '''Decrypts data using RSA (used for authentication)
+
+    Args:
+        private_key: RSA key object (_RSAobj) containing users private key,
+                     used for decrypting
+        ciphertext:  Tuple list(?) representing encrypted message
+
+    Returns:
+        Decrypted message - string
+    '''
     return private_key.decrypt(ciphertext)
 
-'''
-Signs data
-Returns signature for given data and private key
----
-private_key - RSA key object (_RSAobj) containing users private key, used for signing
-data        - Data to sign
-'''
+
 def sign(private_key, data):
-    h = SHA.new()
-    h.update(data)
+    '''Signs data
+
+    Args:
+        private_key: RSA key object (_RSAobj) containing users private key,
+                     used for signing
+        data:        Data to sign
+
+    Returns:
+        Signature for given data and private key
+    '''
+    hash_obj = SHA.new()
+    hash_obj.update(data)
     signer = PKCS1_PSS.new(private_key)
-    return signer.sign(h)
+    return signer.sign(hash_obj)
 
-'''
-Checks signature on data
-Returns True if authentic, False otherwise
----
-public_key - RSA key object (_RSAobj) containing signers public key
-data       - Signed data in question
-signature  - Proposed signature to test
-'''
+
 def unsign(public_key, data, signature):
-    h = SHA.new()
-    h.update(data)
+    '''Checks signature on data
+
+    Args:
+        public_key: RSA key object (_RSAobj) containing signers public key
+        data:       Signed data in question
+        signature:  Proposed signature to test
+
+    Returns:
+        True if authentic, False otherwise
+    '''
+    hash_obj = SHA.new()
+    hash_obj.update(data)
     signer = PKCS1_PSS.new(public_key)
-    return signer.verify(h, signature)
+    return signer.verify(hash_obj, signature)
 
-'''
-Encrypts data using AES Cipher
-Returns encrypted Text
----
-key     - Key used for encrypting. Keys can be 128, 192, or 256 bits long (we will use 256)
-message - Message to encrypt
-'''
+
 def encrypt_message(key, message):
-    iv = Random.new().read(AES.block_size)
-    cipher = AES.new(key, AES.MODE_CFB, iv)
-    pad_len = AES.block_size - (len(message) % AES.block_size)
-    padding = ''.join([chr(pad_len)]*pad_len)
-    return iv + cipher.encrypt(message + padding)
+    '''Encrypts data using AES Cipher
 
-'''
-Decrypts data using AES Cipher
-Returns decrypted Text. Keys can be 128, 192, or 256 bits long (we will use 256)
----
-key        - Key used for decrypting
-ciphertext - Message to decrypt
-'''
+    Args:
+        key:     Key used for encrypting.
+                 Keys can be 128, 192, or 256 bits long (we will use 256)
+        message: Message to encrypt
+
+    Returns:
+        Encrypted Text
+    '''
+    logging.debug('Plaintext to be encrypted:/n%s', message)
+    iv_random = Random.new().read(AES.block_size)
+    cipher = AES.new(key, AES.MODE_CFB, iv_random)
+    pad_len = AES.block_size - (len(message) % AES.block_size)
+    padding = ''.join([chr(pad_len)] * pad_len)
+    encrypted_text = iv_random + cipher.encrypt(message + padding)
+    logging.debug('Encrypted text:/n%s', encrypted_text)
+    return encrypted_text
+
+
 def decrypt_message(key, ciphertext):
-    iv = ciphertext[:AES.block_size]
-    cipher = AES.new(key, AES.MODE_CFB, iv)
+    '''Decrypts data using AES Cipher
+
+    Args:
+        key:        Key used for decrypting
+        ciphertext: Message to decrypt
+
+    Returns:
+        Decrypted Text
+        Keys can be 128, 192, or 256 bits long (we will use 256)
+    '''
+    logging.debug('Ciphertext to be decrypted:/n%s', ciphertext)
+    iv_random = ciphertext[:AES.block_size]
+    cipher = AES.new(key, AES.MODE_CFB, iv_random)
     padded_msg = cipher.decrypt(ciphertext[AES.block_size:])
     pad_len = ord(padded_msg[-1])
-    return padded_msg[:len(padded_msg)-pad_len]
+    decrypted_text = padded_msg[:len(padded_msg) - pad_len]
+    logging.debug('Decrypted text:/n%s', decrypted_text)
+    return decrypted_text
--- crypto_test.py
+++ crypto_test.py
@@ -1,179 +1,173 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 #
-#  test_rsa.py
-#  
+#  crypto_test.py
+#
 #  Copyright 2014 Ryan Foster <phasecorex@gmail.com>
-#  
+#
 #  This program is free software; you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation; either version 2 of the License, or
 #  (at your option) any later version.
-#  
+#
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#  
+#
 #  You should have received a copy of the GNU General Public License
 #  along with this program; if not, write to the Free Software
 #  Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston,
 #  MA 02110-1301, USA.
-#  
-#  
+#
+#
 
 from crypto import *
 
+
 def bad_sign_test():
     print "Generating key pair A..."
-    keyA = generate_key_pair()
+    key_a = generate_key_pair()
     print "Generating key pair B..."
-    keyB = generate_key_pair()
+    key_b = generate_key_pair()
 
-    pubB = get_public_key(keyB)
-    
+    pub_b = get_public_key(key_b)
+
     data = "Signing sample text with key A..."
-    
+
     print data
-    signature = sign(keyA, data)
-    
+    signature = sign(key_a, data)
+
     print "Checking signature with key B (should be false)..."
-    result = unsign(pubB, data, signature)
-    
+    result = unsign(pub_b, data, signature)
+
     print result
+
 
 def sign_test():
     print "Generating key pair..."
-    keyA = generate_key_pair()
-    pubA = get_public_key(keyA)
-    
+    key_a = generate_key_pair()
+    pub_a = get_public_key(key_a)
+
     data = "Signing sample text..."
-    
+
     print data
-    signature = sign(keyA, data)
-    
+    signature = sign(key_a, data)
+
     print "Checking signature..."
-    result = unsign(pubA, data, signature)
-    
+    result = unsign(pub_a, data, signature)
+
     print result
 
-def load_key_sign_test():
-    print "Loading mykey.pem..."
-    keyA = load_key("mykey.pem")
-    pubA = get_public_key(keyA)
-    
-    data = "Signing sample text..."
-    
-    print data
-    signature = sign(keyA, data)
-    
-    print "Checking signature..."
-    result = unsign(pubA, data, signature)
-    
-    print result
 
 def save_key_sign_test():
     print "Generating key pair..."
-    keyA = generate_key_pair()
-    
+    key_a = generate_key_pair()
+
     data = "Signing sample text..."
-    
+
     print data
-    signature = sign(keyA, data)
-    
+    signature = sign(key_a, data)
+
     print "Saving key with password 'hello'..."
-    save_full_key(keyA, "/home/ryan/Desktop/user.pem", "hello")
-    
+    save_full_key(key_a, "/home/ryan/Desktop/user.pem", "hello")
+
     print "Loading key with password 'hell'..."
-    keyB = load_key("/home/ryan/Desktop/user.pem", "hello")
-    pubB = get_public_key(keyB)
-    
+    key_b = load_key("/home/ryan/Desktop/user.pem", "hello")
+    pub_b = get_public_key(key_b)
+
     print "Checking signature..."
-    result = unsign(pubB, data, signature)
-    
+    result = unsign(pub_b, data, signature)
+
     print result
-    
+
+
 def save_key_wrong_pass_test():
     print "Generating key pair..."
-    keyA = generate_key_pair()
-    
+    key_a = generate_key_pair()
+
     data = "Signing sample text..."
-    
+
     print data
-    signature = sign(keyA, data)
-    
+    signature = sign(key_a, data)
+
     print "Saving key with password 'hello'..."
-    save_full_key(keyA, "/home/ryan/Desktop/user.pem", "hello")
-    
+    save_full_key(key_a, "/home/ryan/Desktop/user.pem", "hello")
+
     print "Loading key with password 'hell'..."
     try:
-        keyB = load_key("/home/ryan/Desktop/user.pem", "hell")
-    except:
+        key_b = load_key("/home/ryan/Desktop/user.pem", "hell")
+    except IOError:
         print "Incorrect password..."
         return 'break'
-    
-    pubB = get_public_key(keyB)
-    
+
+    pub_b = get_public_key(key_b)
+
     print "Checking signature..."
-    result = unsign(pubB, data, signature)
-    
+    result = unsign(pub_b, data, signature)
+
     print result
+
 
 def bad_encrypt_test():
     print "Generating key pair A..."
-    keyA = generate_key_pair()
+    key_a = generate_key_pair()
     print "Generating key pair B..."
-    keyB = generate_key_pair()
-    
-    pubA = get_public_key(keyA)
-    pubB = get_public_key(keyB)
-    
+    key_b = generate_key_pair()
+
+    pub_a = get_public_key(key_a)
+    pub_b = get_public_key(key_b)
+
     print "Adding 42+12..."
     print "Encrypting 42 with pubkey B..."
-    encrypted_b = encrypt_auth(pubB, 42)
+    encrypted_b = encrypt_auth(pub_b, 42)
     print "Encrypting 12 with pubkey A..."
-    encrypted_a = encrypt_auth(pubA, 12)
-    
+    encrypted_a = encrypt_auth(pub_a, 12)
+
     print "Decrypting encrypted_a with private key B (should fail)..."
-    numberA = decrypt_auth(keyB, encrypted_a)
+    number_a = decrypt_auth(key_b, encrypted_a)
     print "Decrypting encrypted_b with private key A (should fail)..."
-    numberB = decrypt_auth(keyA, encrypted_b)
-    
+    number_b = decrypt_auth(key_a, encrypted_b)
+
     print "Adding results..."
-    result = numberA + numberB
-    
-    if (result==42+12):
+    result = number_a + number_b
+
+    if (result == 42 + 12):
         print("Result is %s. Something went wrong..." % (result))
     else:
-        print("Result is %s. Yay! We need to figure out how to detect incorrect keys..." % (result))
+        print(
+            "Result is %s. Yay! "
+            "We need to figure out how to detect incorrect keys..." % (result))
+
 
 def encrypt_test():
     print "Generating key pair A..."
-    keyA = generate_key_pair()
+    key_a = generate_key_pair()
     print "Generating key pair B..."
-    keyB = generate_key_pair()
-    
-    pubA = get_public_key(keyA)
-    pubB = get_public_key(keyB)
-    
+    key_b = generate_key_pair()
+
+    pub_a = get_public_key(key_a)
+    pub_b = get_public_key(key_b)
+
     print "Adding 42+12..."
     print "Encrypting 42 with pubkey B..."
-    encrypted_b = encrypt_auth(pubB, 42)
+    encrypted_b = encrypt_auth(pub_b, 42)
     print "Encrypting 12 with pubkey A..."
-    encrypted_a = encrypt_auth(pubA, 12)
-    
+    encrypted_a = encrypt_auth(pub_a, 12)
+
     print "Decrypting encrypted_a with private key A..."
-    numberA = decrypt_auth(keyA, encrypted_a)
+    number_a = decrypt_auth(key_a, encrypted_a)
     print "Decrypting encrypted_b with private key B..."
-    numberB = decrypt_auth(keyB, encrypted_b)
-    
+    number_b = decrypt_auth(key_b, encrypted_b)
+
     print "Adding results..."
-    result = numberA + numberB
-    
-    if (result==42+12):
-        print("Result is %s. Yay!" % (result))
+    result = number_a + number_b
+
+    if result == 42 + 12:
+        print "Result is %s. Yay!" % result
     else:
-        print("Result is %s. Something went wrong..." % (result))
+        print "Result is %s. Something went wrong..." % result
+
 
 def sym_cipher_test():
     key = b'0123456789012345'
@@ -181,12 +175,12 @@ def sym_cipher_test():
     print encrypted
     print decrypt_message(key, encrypted)
 
+
 def get_public_key_test():
     print "Generating key pair..."
-    keyA = generate_key_pair()
-    
+    key_a = generate_key_pair()
+
     print "Public key:"
-    print get_public_key_string(keyA)
+    print get_public_key_string(key_a)
 
-save_key_wrong_pass_test()
-
+encrypt_test()
